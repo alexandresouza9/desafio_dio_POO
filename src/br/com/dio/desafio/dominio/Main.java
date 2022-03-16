959501026b351d3c65f455b5df3a97dc5093698b
alexandresouza9 <alexandresouza9@gmail.com>
2022-03-16 17:44:04 -0300

    first commit

---
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,31 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args){
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Java para iniciantes");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Java para iniciantes");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setNome("Mentoria de Java");
+        mentoria.setDescricao("De Java");
+        mentoria.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+    }
+}
+
+
+
